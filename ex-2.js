4aef73121a59dab68f234858062717713c045afe
zvryn <sarinkanok@gmail.com>
2024-05-29 10:35:22 +0700

    feat:add completed assignment object oriented programming

--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,35 @@
-class Notification { }
+class Notification{
+    constructor (notificationId,createdTime,content,receiver){
+        this.notificationId = notificationId;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.receiver = receiver;
+    }
+    send(){
+        console.log(`Notification has been sent to ${this.receiver}`);
+    };
+}
 
-class EmailNotification { }
 
-class SMSNotification { }
+
+
+class EmailNotification extends Notification {
+    constructor (notificationId,createdTime,content,receiver){
+        super (notificationId,createdTime,content,receiver);
+    }
+ }
+
+class SMSNotification extends Notification {
+    constructor (notificationId,createdTime,content,receiver){
+        super (notificationId,createdTime,content,receiver);
+    }
+
+}
+
+const test1 = new EmailNotification("1234",15.30,"abcde","test1@gmail.com");
+ test1.send();
+
+ const test2 = new SMSNotification("1234",15.30,"abcde","0897654321");
+ test2.send();
+
+ 
